602763384792a00b9aa30025ac829434c4e15595
SmallBowl <awsuhime@gmail.com>
2025-02-06 14:04:40 -0800

    Static

---
+++ IntermediateScripting/Assets/Scripts/Statics/GameStatic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyStatic enemy1 = new EnemyStatic();
+        EnemyStatic enemy2 = new EnemyStatic();
+        EnemyStatic enemy3 = new EnemyStatic();
+
+        int x = EnemyStatic.enemyCount;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ IntermediateScripting/Assets/Scripts/Statics/UtilitiesExampleStatic.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilitiesExampleStatic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = UtilitiesStatic.Add(5, 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
